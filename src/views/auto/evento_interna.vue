c1d9d55f983823cf0faa7c0e69c53da1cec9fbaa
Pablo Papalardo <papalardo94@hotmail.com>
2018-10-17 19:07:06 -0300

    Interna evento

--- src/views/auto/evento_interna.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <cover img="auto/evento/cover-header.jpg"/>
-        <section class="interna">
-
-        </section>
-    </div>
-</template>
--- src/views/petro/evento_interna.vue
+++ src/views/petro/evento_interna.vue
@@ -2,7 +2,16 @@
     <div>
         <cover img="auto/evento/cover-header.jpg"/>
         <section class="interna">
-
+            <div class="grid-container">
+            <div class="grid-x grid-margin-x">
+                <div class="cell text-center">
+                    <h1> Eventos </h1>
+                </div>
+                <div class="cell text-center">
+                    <h2> Fundo para Convergência Estrutural do Mercosul </h2>
+                </div>
+            </div>
+        </div>
         </section>
     </div>
 </template>
