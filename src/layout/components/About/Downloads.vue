de6b9ef90b39f8c4e005aa9bf27a3dc746ee85ba
Ediano Gama <eddy.tadim@gmail.com>
2018-10-22 15:03:59 -0300

    ajustes de abas

--- src/layout/components/About/Downloads.vue
+++ src/layout/components/About/Downloads.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="grix-container">
-        <div class="grid-x grid-padding-xx">
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x">
             <div class="large-6 cell" v-for="(item, key) in list" :key="key">
-                <div class="grid-x grid-padding-x">
-                    <div class="large-6 cell">
-                        <h3> {{ item.title }} </h3>
-                    </div>
-                    <div class="large-6 cell">
-                        <a :href="item.path" class="btn primary"> Baixe aqui <v-icon name="download"/></a>
-                    </div>
-                </div>
+              <div class="grid-x">
+                  <div class="large-6 cell">
+                      <h3> {{ item.title }} </h3>
+                  </div>
+                  <div class="large-6 cell">
+                    <a :href="item.path" target="_blank" class="button button-primary button-download  dark-blue"> Baixe aqui <v-icon name="download"/></a>
+                  </div>
+              </div>
             </div>
         </div>
     </div>
