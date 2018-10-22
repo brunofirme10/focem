3fa87e07e50e537a86b61af5512bd5a6fd993c18
brunofirme10 <bruno10firme@gmail.com>
2018-10-22 16:09:07 -0200

    Footer

--- src/layout/components/FooterBar.vue
+++ src/layout/components/FooterBar.vue
@@ -3,7 +3,7 @@
 <div class="footer">
     <div class="grid-container">
             <div class="grid-x grid-padding-x grid-padding-y align-center text-left">
-                <div class="cell medium-2">
+                <div class="cell medium-4">
                     <div class="grid-x grid-padding-x grid-padding-y align-center text-left">
                         <div class="nav-footer">
                             <ul>
@@ -15,27 +15,29 @@
                         </div>
                     </div>
                 </div>
-                <div class="cell medium-10">
+                <div class="cell medium-8">
                     <div class="grid-x grid-padding-x grid-padding-y align-center text-center">
-                        <div class="cell medium-3">
+                        <div class="cell medium-4">
+                        </div>                        
+                        <div class="cell medium-2">
                             <img src="@/assets/img/logo-abdi.png">
                         </div>
-                        <div class="cell medium-3">
+                        <div class="cell medium-2">
                             <img src="@/assets/img/logo-abdi.png">
                         </div>
-                        <div class="cell medium-3">
+                        <div class="cell medium-2">
                             <img src="@/assets/img/logo-abdi.png">
                         </div>
-                        <div class="cell medium-3">
+                        <div class="cell medium-2">
                             <img src="@/assets/img/logo-abdi.png">
                         </div>
                     </div>
                 </div>
             </div>
         <div class="grid-x grid-padding-x grid-padding-y align-center text-right">
-            <div class="cell medium-8">
+            <div class="cell">
                 <div class="cell medium-4 align-center text-right">
-                            <p>TODOS OS DIREITOS RESERVADOS - ABDI - 2018</p>
+                    <p>TODOS OS DIREITOS RESERVADOS - ABDI - 2018</p>
                 </div>
             </div>
         </div>
